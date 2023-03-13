ca4708962e483689e2d3f52a89361ac48c77acba
Yael Harosh <yaelharosh01@gmail.com>
2023-03-13 15:36:46 -0400

    pass all tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import SideMenu from "./SideMenu";
+import MainContent from "./MainContent";
+import TopMenu from './TopMenu';
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+  <div className="app-container">
+    <TopMenu />
+    <MainContent />
+    <SideMenu />
+  </div>
+  )
 }
 
 export default App;
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,15 @@
 import React from "react";
+import MenuLinks from './MenuLinks';
+import Search from "./Search";
+import Logo from "./Logo";
 
 function SideMenu() {
   return (
-    <aside className="side-bar open">{/* What components go here? */}</aside>
+    <aside className="side-bar open">
+      <Logo />
+      <MenuLinks />
+      <Search />
+    </aside>
   );
 }
 
